49a9d3cb15adedece1ec11de0a7bbae6cf61eeef
Santi <santiago.38@hotmail.com>
2025-08-26 22:35:26 -0300

    resuelto promise

--- ejercicio2.js
+++ ejercicio2.js
@@ -1,7 +1,37 @@
-const prepararPedido = () => {};
+const prepararPedido = async () => {
+  try {
+    await Promise.all([prepararEnsalada(), prepararHamburguesa(), prepararBebida()]);
+    console.log("Pedido completo");
+  } catch (error) {
+    console.error("Error al preparar el pedido:", error);
+  }
+};
 
-const prepararEnsalada = () => {};
+const prepararEnsalada = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("Ensalada lista");
+      resolve();
+    }, 4000);
+  });
+};
 
-const prepararHamburguesa = () => {};
+const prepararHamburguesa = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("Hamburguesa lista");
+      resolve();
+    }, 8000);
+  });
+};
 
-const prepararBebida = () => {};
+const prepararBebida = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("Bebida lista");
+      resolve();
+    }, 2000);
+  });
+};
+
+prepararPedido();
